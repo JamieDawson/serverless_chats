e8920ce7b05f5109ba76aaa0258a0f2669696239
JamieDawson <jamierobertdawson@gmail.com>
2020-09-29 22:32:36 -0700

    updated code

--- packages/hello/serverless_chats.js
+++ packages/hello/serverless_chats.js
@@ -1,34 +1,44 @@
-'use strict';
+// jshint esversion: 9
 
 /**
- * @description Says "Hello, world!" or "Hello, <name>" when the name is provided.
+ * @description null
  * @param {ParamsType} params list of command parameters
  * @param {?string} commandText text message
  * @param {!object} [secrets = {}] list of secrets
  * @return {Promise<SlackBodyType>} Response body
  */
-async function _command(params, commandText, secrets) {
-  const {name = 'World'} = params;
-  // This array is used to store slack blocks.
-  const result = [];
 
-  if (secrets && secrets.helloworld) {
-    result.push({
-      type: 'section',
-      text: {type: 'mrkdwn', text: `I FOUND A SECRET!`}
-    });
-  }
+async function install(pkgs) {
+	pkgs = pkgs.join(' ');
+	return new Promise((resolve, reject) => {
+		const {exec} = require('child_process');
+		exec(`npm install ${pkgs}`, (err, stdout, stderr) => {
+			if (err) reject(err);
+			else resolve();
+		});
+	});
+}
 
-  result.push({
-    type: 'section',
-    text: {type: 'mrkdwn', text: `Hello, ${name}!`}
-  });
+async function _command(params, commandText, secrets = {}) {
+	//rss-parser is a npm package that does not automatically come with commander, so it must be added in install()
+	let packages = ['rss-parser'];
+	await install(packages);
 
-  return {
-    // Or `ephemeral` for private response
-    response_type: 'in_channel', // eslint-disable-line camelcase
-    blocks: result
-  };
+	let Parser = require('rss-parser');
+	let parser = new Parser();
+
+	let feed = await parser.parseURL(
+		'https://feeds.transistor.fm/serverless-chats'
+	);
+
+	var title = feed.items[0].title;
+	var desc = feed.items[0].itunes.subtitle;
+	var chatLink = feed.items[0].link;
+
+	return {
+		response_type: 'in_channel', // or `ephemeral` for private response
+		text: title + '\n' + desc + '\n' + chatLink,
+	};
 }
 
 /**
@@ -36,14 +46,22 @@ async function _command(params, commandText, secrets) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-const main = async args => ({
-  body: await _command(
-    args.params,
-    args.commandText,
-    args.__secrets || {}
-  ).catch(error => ({
-    response_type: 'ephemeral',
-    text: `Error: ${error.message}`
-  }))
+
+const main = async (args) => ({
+	body: await _command(
+		args.params,
+		args.commandText,
+		args.__secrets || {}
+	).catch((error) => ({
+		// To get more info, run `/nc activation_log` after your command executes
+		response_type: 'ephemeral',
+		text: `Error: ${error.message}`,
+	})),
 });
 module.exports = main;
+
+/*
+	console.log(feed.items[0].title); // Gets title of episode!
+	console.log(feed.items[0].itunes.subtitle); //Gets description of episode!
+	console.log(feed.items[0].link); // gets link to episode!
+ */
